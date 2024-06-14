0746901169467f9c633d3ca45e1837f4b7ee4d04
The Sasquatch <hjfarfand@gmail.com>
2024-06-14 17:17:25 -0400

    quienes somos, imagenes centradas testimonios, links de redireccion

--- js/app.js
+++ js/app.js
@@ -1,35 +1,35 @@
   const imgglobaliado = document.getElementById('imgglobaliado');
   imgglobaliado.addEventListener('mouseover', () => {
-    imgglobaliado.src = './img/sucursales/svg/Isologo globaliado color.svg';
+    imgglobaliado.src = './img/sucursales/Isologo globaliado color.svg';
     });
     
     imgglobaliado.addEventListener('mouseout', () => {
-        imgglobaliado.src = './img/sucursales/svg/Isologo globaliado black.svg';
+        imgglobaliado.src = './img/sucursales/Isologo globaliado black.svg';
   });
 
   const imgfuturoverso = document.getElementById('imgfuturoverso');
   imgfuturoverso.addEventListener('mouseover', () => {
-    imgfuturoverso.src = './img/sucursales/svg/FUTUROVERSO COLOR SVG.svg';
+    imgfuturoverso.src = './img/sucursales/FUTUROVERSO COLOR SVG.svg';
     });
     
     imgfuturoverso.addEventListener('mouseout', () => {
-        imgfuturoverso.src = './img/sucursales/svg/FUTUROVERSO BLACK SVG.svg';
+        imgfuturoverso.src = './img/sucursales/FUTUROVERSO BLACK SVG.svg';
   });
 
   const imgpanatramites = document.getElementById('imgpanatramites');
   imgpanatramites.addEventListener('mouseover', () => {
-    imgpanatramites.src = './img/sucursales/svg/ISOLOGO COLOR PANATRAMITES SVG.svg';
+    imgpanatramites.src = './img/sucursales/ISOLOGO COLOR PANATRAMITES SVG.svg';
     });
     
     imgpanatramites.addEventListener('mouseout', () => {
-        imgpanatramites.src = './img/sucursales/svg/ISOLOGO BLACK PANATRAMITES SVG.svg';
+        imgpanatramites.src = './img/sucursales/ISOLOGO BLACK PANATRAMITES SVG.svg';
   });
 
   const nose = document.getElementById('nose');
   nose.addEventListener('mouseover', () => {
-    nose.src = './img/sucursales/svg/Isologo globaliado color.svg';
+    nose.src = './img/sucursales/Isologo globaliado color.svg';
     });
     
     nose.addEventListener('mouseout', () => {
-        nose.src = './img/sucursales/svg/Isologo globaliado black.svg';
+        nose.src = './img/sucursales/Isologo globaliado black.svg';
   });
